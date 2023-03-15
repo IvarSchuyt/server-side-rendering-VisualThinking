f278b18b163dc4ec7e4c3c687ecab06bebc27c50
Ivar Schuyt <112855849+IvarSchuyt@users.noreply.github.com>
2023-03-15 12:33:29 +0100

    push

--- public/script.js
+++ public/script.js
@@ -1,12 +1,13 @@
-let hoverImage = document.querySelector(".template");
+let hoverImage = document.querySelector(".image-template");
 let slideSpan = document.querySelector("span");
 
 hoverImage.addEventListener("mouseover", showCategories);
 hoverImage.addEventListener("mouseout", hideCategories);
 
 function showCategories() {
-  slideSpan.classList.add("categories-shown");  
-  console.log(this);
+  slideSpan.classList.add("categories-shown");
+  // console.log(this); //this verwijst naar het element waarop wordt gemousovert
+  console.log("PARENT", parent.querySelector("span"));
 }
 
 function hideCategories() {
