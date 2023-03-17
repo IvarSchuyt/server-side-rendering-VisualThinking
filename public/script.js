85ac9ee8c82956cdc87257deee1c2fb98a3ef161
Ivar Schuyt <112855849+IvarSchuyt@users.noreply.github.com>
2023-03-17 12:04:49 +0100

    zo kon het ook

--- public/script.js
+++ public/script.js
@@ -1,16 +1,14 @@
 let hoverImages = document.querySelectorAll(".image-template");
 
 hoverImages.forEach((hoverImage) => {
-  hoverImage.addEventListener("mouseover", showCategories);
+  hoverImage.addEventListener("mouseover", function () {
+    // console.log(this); //this verwijst naar het element waarop wordt gemousovert
+    const parent = hoverImage.parentNode;
+    let slideSpan = parent.querySelector("span");
+    slideSpan.classList.add("categories-shown");
+  });
   hoverImage.addEventListener("mouseout", hideCategories);
 });
-
-function showCategories() {
-  // console.log(this); //this verwijst naar het element waarop wordt gemousovert
-  const parent = this.parentNode;
-  let slideSpan = parent.querySelector("span");
-  slideSpan.classList.add("categories-shown");
-}
 
 function hideCategories() {
   const parent = this.parentNode;
